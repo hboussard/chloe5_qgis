0b63f574b424bae9b22fb71590be46b59699ecdd
Daan Guillerme <daan.guillerme@fdc22.com>
2023-11-13 11:04:18 +0100

    Custom raster input widget wrapper update
    Set the default selected raster to an empty raster layer

--- processing/gui/custom_widgets/raster_input/widget_wrapper.py
+++ processing/gui/custom_widgets/raster_input/widget_wrapper.py
@@ -1,41 +1,20 @@
-from pathlib import Path
 from processing.gui.wrappers import (
     RasterWidgetWrapper,
     DIALOG_STANDARD,
 )
-from qgis.core import QgsSettings
-from qgis.PyQt.QtWidgets import QFileDialog
+from qgis.gui import QgsProcessingMapLayerComboBox
+from qgis.core import QgsRasterLayer
 
 
 class ChloeAscRasterWidgetWrapper(RasterWidgetWrapper):
-    """A widget wrapper for a raster layer selection widget."""
+    """A widget wrapper for a raster layer selection widget where there is no default selected layer in the combobox."""
 
     def createWidget(self, dependantWidgetConfig=None):
-        self.fileFilter = "Geotiff (*.tif);;ASCII (*.asc)"
-        return super().createWidget()
-
-    # overiding this method to redefine fileFilter
-    def getFileName(self, initial_value=""):
-        """base class method overide. Shows a file open dialog"""
-        settings = QgsSettings()
-        if Path(initial_value).is_dir():
-            path = initial_value
-        elif Path(initial_value).parent.is_dir():
-            path = Path(initial_value).parent
-        elif settings.contains("/Processing/LastInputPath"):
-            path = str(settings.value("/Processing/LastInputPath"))
-        else:
-            path = ""
-
-        filename, selected_filter = QFileDialog.getOpenFileName(
-            self.widget, self.tr("Select File"), path, self.fileFilter
-        )
-        if filename:
-            settings.setValue(
-                "/Processing/LastInputPath", Path(filename).resolve().parent
-            )
-
-        return filename, selected_filter
+        widget = super().createWidget()
+        if isinstance(widget, QgsProcessingMapLayerComboBox):
+            dummy_layer = QgsRasterLayer()
+            widget.setLayer(dummy_layer)
+        return widget
 
     def postInitialize(self, widgetWrapperList):
         # no initial selection

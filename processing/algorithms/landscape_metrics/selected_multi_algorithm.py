0208b2c4619eb32d6a934809b7252d90746b0cea
Daan Guillerme <daan.guillerme@fdc22.com>
2023-12-05 20:29:57 +0100

    Selected multi algorithm update
    Added a layer input parameter for friction file + added a file filter to points file parameter

--- processing/algorithms/landscape_metrics/selected_multi_algorithm.py
+++ processing/algorithms/landscape_metrics/selected_multi_algorithm.py
@@ -1,6 +1,7 @@
+from typing import Union
 from qgis.core import (
     QgsProcessingParameterDefinition,
-    QgsProcessingParameterNumber,
+    QgsMapLayer,
     QgsProcessingParameterString,
     QgsProcessingParameterFile,
     QgsProcessingParameterEnum,
@@ -9,8 +10,6 @@ from qgis.core import (
 )
 
 from processing.tools.system import isWindows
-
-from ....helpers.helpers import convert_int_to_odd
 
 from ...gui.custom_widgets.constants import CUSTOM_WIDGET_DIRECTORY
 
@@ -140,6 +139,7 @@ class SelectedMultiAlgorithm(ChloeAlgorithm):
                 name=POINTS_FILE,
                 description=self.tr("Point file"),
                 optional=False,
+                fileFilter="CSV (*.csv *.CSV);;TXT (*.txt *.TXT);; DBF (*.dbf *.DBF)",
             )
         )
 
@@ -174,13 +174,23 @@ class SelectedMultiAlgorithm(ChloeAlgorithm):
         self.addParameter(window_shape_param)
 
         # FRICTION FILE
-        friction_file_param = QgsProcessingParameterFile(
-            name=FRICTION_FILE, description=self.tr("Friction file"), optional=True
+        friction_file_param = ChloeRasterParameterFileInput(
+            name=FRICTION_FILE,
+            description=self.tr("Friction file"),
+            optional=True,
         )
-        friction_file_param.setFileFilter("GeoTIFF (*.tif *.TIF);; ASCII (*.asc *.ASC)")
+        friction_file_param.setMetadata(
+            {
+                "widget_wrapper": {
+                    "class": f"{CUSTOM_WIDGET_DIRECTORY}.layer_input.widget_wrapper.ChloeRasterInputWidgetWrapper"
+                }
+            }
+        )
+
         friction_file_param.setFlags(
             friction_file_param.flags() | QgsProcessingParameterDefinition.FlagAdvanced
         )
+        self.addParameter(friction_file_param)
         # ANALYZE TYPE
 
         analyze_type_param = QgsProcessingParameterEnum(
@@ -280,7 +290,15 @@ class SelectedMultiAlgorithm(ChloeAlgorithm):
             self.parameterAsEnum(parameters, WINDOW_SHAPE, context)
         ]
 
-        self.friction_file = self.parameterAsString(parameters, FRICTION_FILE, context)
+        friction_layer: Union[QgsMapLayer, None] = self.parameterAsLayer(
+            parameters, FRICTION_FILE, context
+        )
+        self.friction_file = (
+            friction_layer.source()
+            if friction_layer is not None
+            and self.window_shape == WindowShapeType.FUNCTIONAL.value
+            else ""
+        )
 
         self.window_sizes = self.parameterAsString(parameters, WINDOW_SIZES, context)
 
@@ -342,7 +360,7 @@ class SelectedMultiAlgorithm(ChloeAlgorithm):
         if self.analyze_type == AnalyzeType.WEIGHTED.value:
             properties_lines.append(f"distance_function={str(self.distance_formula)}")
         properties_lines.append(f"shape={str(self.window_shape)}")
-        if self.window_shape == WindowShapeType.FUNCTIONAL.value:
+        if self.friction_file:
             properties_lines.append(f"friction={self.friction_file}")
 
         points_files = format_path_for_properties_file(self.points_file, isWindows())

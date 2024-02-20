a81a7582958b44c38cd03a1609fe2a73f8fc2081
hboussard <hugues.boussard@inrae.fr>
2024-02-20 10:00:10 +0100

    JAVA library

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 CHLOE_JAR_NAME: str = "chloe5-0.0.1.jar"
-CHLOE_JAR_PATH: str = rf"-jar -Xmx4g -Xss8m  bin/{CHLOE_JAR_NAME}"
+CHLOE_JAR_PATH: str = rf"-jar -Xmx2g -Xss8m  bin/{CHLOE_JAR_NAME}"
 CHLOE_WORKING_DIRECTORY_PATH: Path = Path(__file__).parent.parent / "Chloe"
 CHLOE_PLUGIN_PATH: Path = Path(__file__).parent.parent
 
